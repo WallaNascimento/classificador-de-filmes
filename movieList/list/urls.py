47c4fa1b900de4c5721ec1aaa14ae965863893b5
Walla Nascimento <151635113+WallaNascimento@users.noreply.github.com>
2024-03-08 20:15:58 -0300

    Início do CRUD
    Exibir, Cadastrar e Deletar

--- movieList/list/urls.py
+++ movieList/list/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 path('', views.index, name='index'),
-]
+path('addMovie/', views.addMovie, name='addMovie'),
+path('deleteMovie/<str:pk>/', views.deleteMovie, name='deleteMovie'),
+    ]
--- movieList/list/views.py
+++ movieList/list/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
+from list.models import Movie
+
 # Create your views here.
 
 def index(request):
-    context = {}
+    movie = Movie.objects.all()
+    context = {'movie':movie}
     return render(request, 'index.html', context)
     
+def addMovie(request):
+    name = request.POST['fname']
+    duration = request.POST['fduration']
+    newMovie = Movie(
+        name = name,
+        duration = duration,
+    )
+    newMovie.save()
+    return redirect('http://127.0.0.1:8000/')
+
+def deleteMovie(request, pk):
+    movie = Movie.objects.get(id=pk)
+    movie.delete()
+    return redirect('http://127.0.0.1:8000/')
