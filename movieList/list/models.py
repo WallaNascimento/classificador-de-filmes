57a9fb7f3a92bc5b5ee7b3ee8d1f70026bf65b98
Walla Nascimento <151635113+WallaNascimento@users.noreply.github.com>
2024-04-18 08:44:01 -0300

    Genre
    Visualização e inserção de géneros ao filme

--- movieList/list/models.py
+++ movieList/list/models.py
@@ -6,6 +6,19 @@ class Movie(models.Model):
     name = models.CharField(max_length=100)
     duration = models.IntegerField()
   # date = models.DateField
-  
+      
     def __str__(self):
         return self.name
+    
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+class GenreMovie(models.Model):
+    movie=models.ForeignKey(Movie, on_delete=models.CASCADE)
+    genre=models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.movie.name + "  -  " + self.genre.name
--- movieList/list/views.py
+++ movieList/list/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, redirect
+from django.http import JsonResponse
 
 from django.http import HttpResponse
-from list.models import Movie
+from list.models import Movie, Genre, GenreMovie
 
 # Create your views here.
 
 def index(request):
     movie = Movie.objects.all()
-    context = {'movie':movie}
+    genre = Genre.objects.all()
+    genremovie = GenreMovie.objects.all()
+    
+    context = {'movie':movie, 
+        'genre':genre,
+        'genremovie':genremovie,
+               }
     return render(request, 'index.html', context)
     
 def addMovie(request):
@@ -19,7 +26,7 @@ def addMovie(request):
     )
     newMovie.save()
     return redirect('http://127.0.0.1:8000/')
-
+        
 def deleteMovie(request, pk):
     movie = Movie.objects.get(id=pk)
     movie.delete()
@@ -40,3 +47,40 @@ def updateMovie(request, pk):
     
     movie.save()
     return redirect('http://127.0.0.1:8000/')
+
+def addGenre(request):
+    name = request.POST['']
+    newGenre = Genre (
+        name = name,
+    )
+    newGenre.save()
+        
+    return redirect('http://127.0.0.1:8000/')
+
+def getMovie(request, pk):
+    movie = Movie.objects.get(id=pk)
+    list_genre_movie = GenreMovie.objects.filter(movie__id=pk)
+    genres = Genre.objects.all()
+    #generos = Genero.objects.filter(id=list_genero_filme)
+    #print(list_genero_filme)
+    context = {
+        'genres': genres,
+        'movie': movie,
+        'list_genre_movie':list_genre_movie}
+    return render(request, 'showGenreMovie.html', context)
+
+
+def addGenreMovie(request, pk):
+    movie = Movie.objects.get(id=pk)
+    genreId = request.POST['genre']
+    genre = Genre.objects.get(id=genreId)
+    
+    newGenreMovie = GenreMovie (
+        movie = movie,
+        genre = genre,
+
+    )
+    newGenreMovie.save()
+    
+    return redirect('http://127.0.0.1:8000/')
+    
